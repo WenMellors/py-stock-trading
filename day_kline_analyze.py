05ad56aa819b14bc6954431b81cf92d8d4e27956
MellesJ <842595644@qq.com>
2023-02-27 23:19:47 +0800

    update get data with akshare

--- day_kline_analyze.py
+++ day_kline_analyze.py
@@ -1,12 +1,51 @@
 # 爬取股票的短期 K 线图（日图），并进行匹配是否出现特定的模式
-from api import query_k_line
-from response_parser import parse_kline_data
+import akshare
+import json
+from datetime import datetime, timedelta
 
-stock_code = 'SH603799'
+with open('./data/my_stock_follows.json', 'r') as f:
+    my_stock_dict = json.load(f)
 
-k_line_data = query_k_line(stock_code=stock_code, period='day')
+# 参数控制
+time_window_size = 100 # 因为 k 线分析都是比较近的数据分析，感觉不用爬取很远的数据
+
+stock_code = '603799'
+# 定义爬取的时间窗口，[start, end]
+end_datetime = datetime.now()
+end_date = end_datetime.strftime('%Y%m%d')
+start_datetime = end_datetime - timedelta(days=time_window_size)
+start_date = start_datetime.strftime('%Y%m%d')
+# 这里我们使用前复权（因为 k 线分析主要是看盘）
+stock_data_hist = akshare.stock_zh_a_hist(symbol=stock_code, period='daily', start_date=start_date, end_date=end_date,
+                                          adjust='qfq')
+stock_data_hist['MA5'] = stock_data_hist['收盘'].rolling(5).mean()
+stock_data_hist['MA10'] = stock_data_hist['收盘'].rolling(10).mean()
+stock_data_hist['MA20'] = stock_data_hist['收盘'].rolling(20).mean()
+
+# 这里我们需要先判断当前股价的大体走势，是在上升期还是下降期
+
 # 解析请求到的 k 线数据
 kline_list = parse_kline_data(k_line_data)
+
+# 这里还是维护一个变量吧，不然不好可能后续不好处理
+# 但其实也可以加载一个预训练语料库，把中文转成嵌入向量（因为命名包含了正负情感？）
+k_line_pattern = {
+    '希望之星': 0,
+    '反攻': 1,
+    '曙光初现': 2,
+    '旭日东升': 3,
+    '平底': 4,
+    '圆底': 5,
+    '跳空三连阴': 6,
+    '上升抵抗形': 7
+}
+
+signal_dict = {
+    '见底信号': 0,
+    '买进信号': 1,
+
+}
+
 
 
 def kline_pattern_match(data):
@@ -15,6 +54,9 @@ def kline_pattern_match(data):
     :param data: 历史 kline 数组
     :return: 匹配到的k线组合 string
     """
+    # 判断一下是上涨周期，还是下跌周期
+    # 就用均线来判断吧
+
     # if-else
     if data[-1].change_range > 0 and data[-2].mode == '十字星' and data[-3].change_range < 0:
         if data[-1].close > data[-3].close:
@@ -58,6 +100,11 @@ def kline_pattern_match(data):
             (data[-3].change_range < 0 and data[-3].open < data[-4].close):
         # 跳空三连阴
         return '跳空三连阴', '见底信号', '看涨'
+    if (0 < data[-1].change_range < 1.5) and (0 < data[-2].change_range < 1.5) and \
+            (0 < data[-2].change_range < 1.5) and (data[-1].high  > data[-2].high > data[-3].high):
+        return '红三兵', '买进信号', '看涨'
+    if (data[-1])
+
     # Default
     return '无模式', '无信号', '无'
 
